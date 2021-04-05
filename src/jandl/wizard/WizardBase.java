8aac8e951ce30a37481aa708ea933ec706722606
PJandl <pjandl@gmail.com>
2021-04-05 17:23:18 -0300

    Add files via upload
    Inclusão de comentários de documentação na classe base destinada a criação de assistentes GUI.

--- src/jandl/wizard/WizardBase.java
+++ src/jandl/wizard/WizardBase.java
@@ -22,42 +22,111 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+/**
+ * Classe base para criação de assistentes, janelas padronizadas que
+ * podem ser encadeadas para formar uma aplicações capazes de colher
+ * dados do usuário, efetuando seu processamento/tratamento, para
+ * realização de tarefas simples.
+ * É uma customização da class {@link javax.swing.JFrame} , que oferece um 
+ * painel lateral esquerdo para exibição de imagem customizável,
+ * uma área central que podem conter um componente de entrada
+ * especializado ou uma combinação deles, além de um painel inferior
+ * contendo dois botões para navegação para frente e para trás numa
+ * sequência encadeável de assistentes.
+ *
+ * @author pjandl
+ */
 public class WizardBase extends JFrame {
 	/**
-	 * serialVersionUID = YYYYMMDD
+	 * Número de versão serial único do framework, no formato YYYYMMDD.
 	 */
 	public static final long serialVersionUID = 20210331L;
 	/**
-	* autonumbering
+	* Autonumeração dos assistentes (wizards).
 	*/
 	private static int wizardNumber = 0;
-	public static long version;
-
+	/**
+	 * Rótulo para exibição da imagem lateral esquerda dos assistentes.
+	 */
 	private JLabel lImage;
-	private JButton bBack, bNext;
+	/**
+	 * Botão para navegação para assistente anterior.
+	 */
+	private JButton bBack;
+	/**
+	 * Botão para navegação para assistente posterior.
+	 */
+	private JButton bNext;
+	/**
+	 * Painel de rolagem para suportar conteúdo maior do que a área central
+	 * disponível do assistente (que tem dimensões fixas: 640 x 480 pixels).
+	 */
 	private JScrollPane scrollPane;
-	private WizardBase backWizard, nextWizard;
-	private boolean end;
+	/**
+	 * Referência para assistente anterior.
+	 */
+	private WizardBase backWizard;
+	/**
+	 * Referência para assistente posterior.
+	 */
+	private WizardBase nextWizard;
+	/**
+	 * Lista de objetos pré processadores do assistente.
+	 */
 	private ArrayList<Consumer<WizardBase>> preProcessorList;
+	/**
+	 * Lista de objetos pós processadores do assistente.
+	 */
 	private ArrayList<Consumer<WizardBase>> postProcessorList;
 	
+	/**
+	 * Construtor default. Define título padrão do assistente, com imagem lateral
+	 * padrão (que não preenche toda janela). 
+	 */
 	public WizardBase(){
 		this("WizardBase", null, false);
 	}
 
+	/**
+	 * Construtor parametrizado com título do assistente. Usa imagem lateral
+	 * padrão (que não preenche toda janela).
+	 * 
+	 * @param title título da janela do assistente
+	 */
 	public WizardBase(String title){
 		this(title, null, false);
 	}
 	
+	/**
+	 * Construtor parametrizado com título do assistente e nome do arquivo para
+	 * imagem lateral (que não preenche toda janela). O nome do arquivo pode ser
+	 * fornecido de maneira relativa ou absoluta, seguindo as convenções da 
+	 * plataforma em uso.
+	 * 
+	 * @param title título da janela do assistente
+	 * @param imageFile nome do arquivo da imagem lateral.
+	 */
 	public WizardBase(String title, String imageFile){
 		this(title, imageFile, false);
 	}
 
+	/**
+	 * Construtor parametrizado com título do assistente, nome do arquivo para
+	 * imagem lateral e indicação de modo de preenchimento. O nome do arquivo 
+	 * pode ser fornecido de maneira relativa ou absoluta, seguindo as convenções
+	 * da plataforma em uso. O modo de preenchimento permite definir que a imagem
+	 * usada preenche toda a janela.
+	 * 
+	 * @param title título da janela do assistente
+	 * @param imageFile nome do arquivo da imagem lateral.
+	 * @param fill modo de preenchimento: <b>true</b> indica que imagem preenche janela,
+	 *             <b>false</b> indica imagem lateral (30% da janela) 
+	 */
 	public WizardBase(String title, String imageFile, boolean fill){
 		super(title);
 		this.setName("Wizard" + (++wizardNumber));
 		System.out.println("WizardBase.<init>(" + title + "), " + this.getName());
-		// Lateral or Center Image
+		// Imagem lateral ou central
 		this.setImage(imageFile);
 		scrollPane = new JScrollPane(lImage);
 		if (fill) {
@@ -70,7 +139,7 @@ public class WizardBase extends JFrame {
 			scrollPane.setPreferredSize(new Dimension(280, 440));
 			add(scrollPane, "West");
 		}
-		// Botton Button Panel
+		// Panel inferior de botões
 		JPanel bbp = new JPanel(new BorderLayout());
 		bbp.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
 		bBack = new JButton("Anterior");
@@ -90,9 +159,11 @@ public class WizardBase extends JFrame {
 		bbp.add(bBack, "West");
 		bbp.add(bNext, "East");
 		add(bbp, "South");
-		// Ajustes
-		end = true;
-		setSize(640,480);
+		// Listas de listeners para pré e pós processamento da janela
+		preProcessorList = new ArrayList<>();
+		postProcessorList = new ArrayList<>();
+		// Ajustes gerais
+		setSize(640, 480);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		addWindowListener(new WindowAdapter() {
@@ -109,33 +180,61 @@ public class WizardBase extends JFrame {
 				 }
 			}
 		});
-		bbpLinkCheck();
-		preProcessorList = new ArrayList<>();
-		postProcessorList = new ArrayList<>();
+		buttonsLinkCheck();
 	}
 
+	/**
+	 * Adiciona listener de pós processamento à janela. Todos os listener adicionados
+	 * serão executados na finalização de uma página, ou seja, após o acionamento de
+	 * seu botão Próximo.
+	 * Não existe pós-processamento para a janela final (última).
+	 * 
+	 * @param processor implementação da interface funcional Consumer<WizardBase>.
+	 */
 	public void addPostProcessor(Consumer<WizardBase> processor) {
 		if (processor != null) {
 			postProcessorList.add(processor);
 		}
 	}
 
+	/**
+	 * Adiciona listener de pré processamento à janela. Todos os listener adicionados
+	 * serão executados na inicialização de uma página, ou seja, antes de sua exibição
+	 * em decorrência do acionamento do botão Próximo da página anterior.
+	 * Não existe pré-processamento para a janela inicial (primeira).
+	 * 
+	 * @param processor implementação da interface funcional Consumer<WizardBase>.
+	 */
 	public void addPreProcessor(Consumer<WizardBase> processor) {
 		if (processor != null) {
 			preProcessorList.add(processor);
 		}
 	}
 
+	/**
+	 * Define a janela imediatamente anterior na qual esta janela está encadeada.
+	 * 
+	 * @param link referência para janela anterior
+	 * @return referência para janela anterior, permitindo uso encadeado deste método.
+	 */
 	public WizardBase backWizard(WizardBase link) {
 		backWizard = link;
 		if (link != null) {
 			link.nextWizard = this;
-			link.bbpLinkCheck();
+			link.buttonsLinkCheck();
 		}
-		this.bbpLinkCheck();
-		return this;
+		this.buttonsLinkCheck();
+		return link;
 	}
 
+	/**
+	 * Programação das ações realizadas em decorrência do acionamento do botão
+	 * Anterior:
+	 * <ol>
+	 * <li>reposicionamento e exibição da janela anterior (se registrada).</li>
+	 * </ol>
+	 * @param evt evento gerado pelo processador de eventos Swing
+	 */
 	protected void bBackClick(ActionEvent evt) {
 		System.out.println(this.getName() + ".bBackClick()");
 		backWizard.setLocation(this.getLocation());
@@ -143,19 +242,34 @@ public class WizardBase extends JFrame {
 		this.setVisible(false);
 	}
 
-	private void bbpLinkCheck() {
+	/**
+	 * Método interno para ativação/inativação dos botões de navegação
+	 * conforme o encadeamento das janelas.
+	 */
+	private void buttonsLinkCheck() {
 		bBack.setVisible(backWizard != null);
-		if (this.end) {
-			bNext.setVisible(true);
+		bNext.setVisible(true);
+		if (this.nextWizard == null) {
 			bNext.setText("Fim");
 		} else {
-			bNext.setVisible(nextWizard != null);
 			bNext.setText("Pr\u00f3ximo");
 		}
 	}
 
+	/**
+	 * Programação das ações realizadas em decorrência do acionamento do botão
+	 * Próximo:
+	 * <ol>
+	 * <li>execução dos pós-processadores registrados para esta janela;</li>
+	 * <li>execução dos pré-processadores registrados para a próxima janela 
+	 * (se registrada);</li>
+	 * <li>reposicionamento e exibição da próxima janela (se registrada);</li>
+	 * <li>encerramento da aplicação se não existe próxima janela registrada.</li>
+	 * </ol>
+	 * @param evt evento gerado pelo processador de eventos Swing
+	 */
 	protected void bNextClick(ActionEvent evt) {
-		if (end) {
+		if (this.nextWizard == null) {
 			System.out.println("WizardBase.bNextClick() --> End");
 			System.exit(0);
 		} else {
@@ -179,25 +293,44 @@ public class WizardBase extends JFrame {
 		}
 	}
 
+	/**
+	 * Permite o acionamento programático do botão Anterior.
+	 */
 	public void doBackClick() {
 		bBackClick(null);
 	}
 
+	/**
+	 * Permite o acionamento programático do botão Próximo.
+	 */
 	public void doNextClick() {
 		bNextClick(null);
 	}
 
+	/**
+	 * Define a janela imediatamente posterior na qual esta janela está encadeada.
+	 * 
+	 * @param link referência para janela posterior
+	 * @return referência para janela posterior, permitindo uso encadeado deste método.
+	 */
 	public WizardBase nextWizard(WizardBase link) {
 		nextWizard = link;
 		if (link != null) {
 			link.backWizard = this;
-			link.bbpLinkCheck();
+			link.buttonsLinkCheck();
 		}
-		end = link == null;
-		this.bbpLinkCheck();
+		this.buttonsLinkCheck();
 		return link;
 	}
 
+	/**
+	 * Permite a definição ou redefinição da imagem lateral da janela do assistente.
+	 * A imagem é colocada num painel de rolagem, permitindo a exibição de imagens 
+	 * maiores do que o espaço efetivamente disponível.
+	 *  
+	 * @param imageFile nome do arquivo (pode incluir caminho) correspondente à imagem
+	 * PNG, JPG ou GIF a ser exibida pelo assistente.
+	 */
 	public void setImage(String imageFile) {
 		System.out.println("setImage(" + imageFile + ")");
 		if (imageFile == null) {
@@ -221,7 +354,6 @@ public class WizardBase extends JFrame {
 				if (scrollPane != null) scrollPane.setViewportView(lImage);
 				return;
 			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 //			if (file.exists()) {
